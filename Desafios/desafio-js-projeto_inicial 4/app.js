e91b513040c6718e060baf98ff83f84896d3d7be
Biel <157547002+biel-sebold@users.noreply.github.com>
2025-05-09 21:01:19 -0300

    .

---
+++ Desafios/desafio-js-projeto_inicial 4/app.js
@@ -0,0 +1,25 @@
+// //-------------------------------------------------------------------DESAFIO 1----------------------------------------------------------------------------------------
+// //Crie um programa que utilize o console.log para exibir uma mensagem de boas-vindas.
+
+// console.log('Boas vindas');
+
+
+
+// //------------------------------------------------------------------DESAFIO 2---------------------------------------------------------------------------------------
+// //Crie uma variável chamada "nome" e atribua a ela o seu nome. Em seguida, utilize o console.log para exibir a mensagem "Olá, [seu nome]!" no console do navegador.
+
+// let nome = 'Gabriel';
+// console.log(`Olá, ${nome}`);
+
+
+
+//----------------------------------------------------------------DESAFIO 3----------------------------------------------------------------------------------------
+//Crie uma variável chamada "nome" e atribua a ela o seu nome. Em seguida, utilize o alert para exibir a mensagem "Olá, [seu nome]!" .
+
+let nome = 'Gabriel';
+alert(`Olá, ${nome}`);
+
+
+
+//---------------------------------------------------------------DESAFIO 4--------------------------------------------------------------------------------------------
+//
