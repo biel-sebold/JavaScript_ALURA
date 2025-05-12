5ca0724b53faecdf5862769c002ff99a558ab789
Biel <157547002+biel-sebold@users.noreply.github.com>
2025-05-12 18:57:00 -0300

    .

--- Exercícios/app.js
+++ Exercícios/app.js
@@ -1,36 +1,13 @@
-// GABARITO
-// let numeroSecreto = parseInt(Math.random() * 11)
-// let tentativas = 1
-// let chute
-
-// alert('Boas vindas ao jogo do número secreto')
-
-// while (chute != numeroSecreto) {
-//     let chute = prompt('Escolha um número entre 1 e 10')
-//     if (chute == numeroSecreto) {
-//         break
-//     } else {
-//         if (chute > numeroSecreto) {
-//             alert('O número secreto é menor')
-//         } else {
-//             alert('O número secreto é maior')
-//         }
-//     }
-//     tentativas++
-// }
-
-// let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa' 
-// alert(`O número secreto era ${numeroSecreto} e você acertou com apenas ${tentativas} ${palavraTentativa}`)
-
 alert('Boas vindas ao jogo do número secreto');
-let numeroSecreto = parseInt(Math.random() * 30) + 1;
+let numeroMaximo = 1000;
+let numeroSecreto = parseInt(Math.random() * numeroMaximo + 1);
 console.log(`Número secreto: ${numeroSecreto}`);
 
 let chute;
 let tentativas = 1;
 
 while (chute != numeroSecreto) {
-    chute = prompt('Escolha um número entre 1 e 30');
+    chute = prompt(`Escolha um número entre 1 e ${numeroMaximo}:`);
     //verifica o valor do chute após a entrada do usuário
     console.log('Valor do chute: ', chute);
     //verifica a comparação entre "chute" e "numeroSecerto"
