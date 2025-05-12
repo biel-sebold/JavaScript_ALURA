448c7feb7e3973fa39b7cbf885b7a96697d50f63
Biel <157547002+biel-sebold@users.noreply.github.com>
2025-05-11 21:56:16 -0300

    .

--- Desafios/desafio-js-projeto_inicial 4/app.js
+++ Desafios/desafio-js-projeto_inicial 4/app.js
@@ -1,25 +1,130 @@
-// //-------------------------------------------------------------------DESAFIO 1----------------------------------------------------------------------------------------
+// //---------------------------------------DESAFIO 1---------------------------------
 // //Crie um programa que utilize o console.log para exibir uma mensagem de boas-vindas.
 
-// console.log('Boas vindas');
+console.log('Boas vindas');
 
 
 
-// //------------------------------------------------------------------DESAFIO 2---------------------------------------------------------------------------------------
+// //--------------------------------------DESAFIO 2--------------------------------------------------------------
 // //Crie uma variável chamada "nome" e atribua a ela o seu nome. Em seguida, utilize o console.log para exibir a mensagem "Olá, [seu nome]!" no console do navegador.
 
 // let nome = 'Gabriel';
-// console.log(`Olá, ${nome}`);
+// console.log(`Olá, ${nome}!`);
 
 
 
-//----------------------------------------------------------------DESAFIO 3----------------------------------------------------------------------------------------
-//Crie uma variável chamada "nome" e atribua a ela o seu nome. Em seguida, utilize o alert para exibir a mensagem "Olá, [seu nome]!" .
+// //-------------------------------------------------DESAFIO 3-----------------------------------------------------
+// //Crie uma variável chamada "nome" e atribua a ela o seu nome. Em seguida, utilize o alert para exibir a mensagem "Olá, [seu nome]!" .
 
-let nome = 'Gabriel';
-alert(`Olá, ${nome}`);
+// let nome = 'Gabriel';
+// alert(`Olá, ${nome}!`);
 
 
 
-//---------------------------------------------------------------DESAFIO 4--------------------------------------------------------------------------------------------
-//
+// //-------------------------------------------DESAFIO 4-----------------------------------------------------------
+// //Utilize o prompt e faça a seguinte pergunta: Qual a linguagem de programação que você mais gosta?. Em seguida, armazene a resposta em uma variável e mostre no console do navegador.
+
+// let resposta = prompt("Qual é a linguagem de programação que você mais gosta?");
+// console.log(resposta);
+
+
+
+// //-------------------------------------------DESAFIO 5-----------------------------------------------------------
+// //Crie uma variável chamada "valor1" e outra chamada "valor2", atribuindo a elas valores numéricos de sua escolha. Em seguida, realize a soma desses dois valores e armazene o resultado em uma terceira variável chamada "resultado". Utilize o console.log para mostrar a mensagem "A soma de [valor1] e [valor2] é igual a [resultado]." no console.
+
+// let valor1 = 1;
+// let valor2 = 2
+// let resultado = valor1+valor2;
+
+// console.log(`A soma de ${valor1} e ${valor2} é igual a ${resultado}.`);
+
+
+
+// //------------------------------------------DESAFIO 6------------------------------------------------------------
+// //Crie uma variável chamada "valor1" e outra chamada "valor2", atribuindo a elas valores numéricos de sua escolha. Em seguida, realize a subtração desses dois valores e armazene o resultado em uma terceira variável chamada "resultado". Utilize o console.log para mostrar a mensagem "A diferença entre [valor1] e [valor2] é igual a [resultado]." no console.
+
+// let valor1 = 1;
+// let valor2 = 2;
+// let resultado = valor1-valor2;
+
+// console.log(`A diferença entre ${valor1} e ${valor2} é igual a ${resultado}.`);
+
+
+
+// //-------------------------------------DESAFIO 7----------------------------------------------------------------
+// //Peça ao usuário para inserir sua idade com prompt. Com base na idade inserida, utilize um if para verificar se a pessoa é maior ou menor de idade, exibindo uma mensagem apropriada no console.
+
+// let idade = prompt("Informe a sua idade:");
+
+// if (idade >= 18) {
+//     console.log(`Você é maior de idade (${idade} anos)`);
+// } else {
+//     console.log(`Você é menor de idade (${idade} anos)`);
+// }
+
+
+
+// //------------------------------------------------DESAFIO 8------------------------------------------------------
+// //Crie uma variável "numero" e peça um valor com prompt verifique se é positivo, negativo ou zero. Use if-else para imprimir a respectiva mensagem.
+
+// var numero = parseFloat(prompt("Informe um número:"));
+
+// if (numero > 0) {
+//     console.log("O número informado é positivo");
+    
+// } else if(numero < 0) {
+//     console.log("O número informado é negativo");
+
+// } else {
+//     console.log("O número informado foi o zero.");
+// }
+
+
+
+// //--------------------------------------------DESAFIO 9----------------------------------------------------------
+// //Use um loop while para imprimir os números de 1 a 10 no console.
+
+// let numero = 0;
+
+// while (numero <= 10) {
+//     console.log(numero);
+//     numero++; 
+// }
+
+
+
+// //----------------------------------------DESAFIO 10----------------------------------------------------------
+// //Crie uma variável "nota" e atribua um valor numérico a ela. Use if-else para determinar se a nota é maior, menor ou igual a 7 e exiba "Aprovado" ou "Reprovado" no console.
+
+// let nota = 9.6;
+
+// if (nota >= 7) {
+//     console.log("Aprovado!");
+// } else {
+//     console.log("Reprovado!");   
+// }
+
+
+
+// //--------------------------------------DESAFIO 11---------------------------------------------------------
+// //Use o Math.random para gerar qualquer número aleatório e exiba esse número no console.
+
+// let numeroAleatório = Math.random();
+// console.log(`Número sorteado: ${numeroAleatório}`);
+
+
+
+// //---------------------------------------------DESAFIO 12-----------------------------------------------------
+// //Use o Math.random para gerar um número inteiro entre 1 e 10 e exiba esse número no console.
+
+// let numeroAleatório = parseInt(Math.random() * 10) + 1;
+// console.log(`Número sorteado: ${numeroAleatório}`);
+
+
+
+// //----------------------------------------------DESAFIO 13-----------------------------------------------------
+// //Use o Math.random para gerar um número inteiro entre 1 e 1000 e exiba esse número no console.
+
+// let numeroAleatório = parseInt(Math.random() * 1000) + 1;
+// console.log(`Número sorteado: ${numeroAleatório}`);
+
